13026d4a1d33d332c2abcbd57ccc4512d7d8d594
Henrique Miranda <enrque.ns@gmail.com>
2020-03-31 17:39:47 -0300

    :hammer: refac: create vaccinate function

--- src/content.js
+++ src/content.js
@@ -1,25 +1,23 @@
 const REX = /(corona|covid)((-| )?(v(i|í)rus|19))?/gi;
 
-const ignoreTag = ['script', 'noscript', 'source', 'style'];
+const ignoreTag = ["script", "noscript", "source", "style"];
 
-const [body] = document.getElementsByTagName('body');
+function vaccinate(element) {
+  if (ignoreTag.includes(element.localName)) return;
+
+  const text = element.textContent;
+
+  if (text) {
+    element.textContent = text.replace(REX, "💉");
+  }
+}
 
 function findMatche(element) {
-  if (!element.childElementCount) {
-    if(ignoreTag.includes(element.localName)) return;
-
-    const text = element.textContent;
-
-    if (text) {
-      element.textContent = text.replace(REX, '💉');
-    }
-
-    return;
-  }
-
-  for (el of element.children) findMatche(el);
+  if (!element.childElementCount) vaccinate(element);
+  
+  else for (el of element.children) findMatche(el);
 }
 
 chrome.runtime.onMessage.addListener(req => {
-  if(req.message === 'load') findMatche(body);
+  if (req.message === "load") findMatche(document.body);
 });
